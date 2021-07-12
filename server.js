d8e436ffed23dc9a5477326a71caafb2d0e028a8
Adam <agrgic>
2021-07-12 13:08:01 -0500

    update

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 
+// routes 
+const usersRoutes = require('./routes/users');
+
 const app = express();
 const PORT = 5000;
 
+// old: deprecated 
 app.use(bodyParser.json());
 
+app.use('/users', usersRoutes);
 
+
+// making a request to root route (localhost:PORT) === '/'
 app.get('/', (req, res) => {
     console.log('[TEST]!');
 
